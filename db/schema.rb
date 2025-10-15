18d03347d4471cdca0b51ee268cbfc942690eb61
maricarmen <igmaricarmenorozco7@gmail.com>
2025-10-14 21:53:06 -0600

    Generando migraciones a la base de datos tests y la parte de desarrollo

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_15_034336) do
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_uca1400_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.string "password"
+    t.string "email"
+    t.boolean "programador"
+    t.date "birthday"
+    t.time "work_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
